3291f7397a02599d4dd65578ee23daf25bb15b77
may489 <xyaolong@gmail.com>
2025-09-05 10:08:21 +0800

    Site updated: 2025-09-05 10:08:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/04/Hexo安装安知鱼主题/","2025/09/02/my-second-blog/","2025/09/02/test-my-site/","2025/09/02/hello-world/"];function toRandomPost(){
+var posts=["2025/09/04/Hexo安装安知鱼主题/","2025/09/05/再来一篇/","2025/09/05/Hugo搭建个人博客网站/","2025/09/04/hello-world/","2025/09/02/my-second-blog/","2025/09/02/test-my-site/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
